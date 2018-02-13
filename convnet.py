04fbe8064b5c89dd478a97931af8f047deb1f4a9
Rich Ormiston <rich.ormiston@dgx-1.dcs.ligo-wa.caltech.edu>
2018-02-13 12:52:54 -0800

    2D convolutional neural network with continuous output

--- convnet.py
+++ convnet.py
@@ -1,37 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import matplotlib
 matplotlib.use("agg")
 import matplotlib.pyplot as plt
-
 import numpy as np
 import scipy.io as sio
+import os
+import cdms_lib
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 
-import cdms_funcs
-cdms_funcs.set_plot_style()
 
-import os
+###################
+# Set Environment #
+###################
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['TF_MIN_GPU_MULTIPROCESSOR_COUNT'] = '5'
-
+cdms_lib.set_plot_style()
 
 #################
 # Global params #
-################
+#################
 batch_size = 1
 fs         = 512
-epochs     = 500
+epochs     = 1000
 verbose    = 1
 optimizer  = 'adam'
 
-######################################################
-# Build dataset and split data into 64, 32s segments #
-######################################################
-# full_data  = sio.loadmat('cdms_event_data.mat')['data']
+#############################################
+# Get dataset, normalize filter and reshape #
+#############################################
 full_data = sio.loadmat('short_data.mat')['data']
-prep_data, dd = cdms_funcs.prepare_cdms(full_data)
+prep_data, dd = cdms_lib.prepare_cdms(full_data, Filter=True)
 cnn_data = prep_data.reshape(prep_data.shape[0],
                              prep_data.shape[1],
                              prep_data.shape[2], 1)
@@ -45,12 +47,13 @@ y_train = cnn_data[:t_frac, 0, 0 , :]
 x_test  = cnn_data[t_frac:, :, 1:, :]
 y_test  = cnn_data[t_frac:, 0, 0 , :]
 
+# reshape the target data
 y_train = y_train.reshape(y_train.shape[0], 1)
 y_test  = y_test.reshape(y_test.shape[0], 1)
 
-##########################
-# Build VGG-like ConvNet #
-##########################
+#####################
+# Build the ConvNet #
+#####################
 input_shape = (x_train.shape[1], x_train.shape[2], x_train.shape[3])
 model = Sequential()
 
@@ -61,7 +64,11 @@ model.add(Dropout(0.5))
 
 model.add(Flatten())
 model.add(Dense(64))
+model.add(Dense(128))
+model.add(Dense(32))
+model.add(Dense(16))
 model.add(Dense(8))
+model.add(Dense(16))
 model.add(Dense(1, activation='tanh'))
 
 model.compile(loss='mse', optimizer=optimizer)
@@ -78,34 +85,38 @@ print('RMSE: {}'.format(score))
 #############################
 yhat = model.predict(x_test)
 yhat = yhat.reshape(yhat.shape[0])
-bkgd = np.zeros_like(yhat)
-tar  = y_test.reshape(len(yhat))
 
-##################
-# Make raw plots #
-##################
-plt.plot(range(len(tar)), tar,  label='target')
-plt.plot(range(len(tar)), yhat, label='prediction')
+##############
+# Make plots #
+##############
+y_test = y_test.reshape(len(y_test))
+plt.plot(range(len(yhat)), y_test,  label='target')
+plt.plot(range(len(yhat)), yhat, label='prediction')
 plt.legend(loc='upper right')
 plt.title('CDMS Energy Prediction')
 plt.xlabel('Event')
-plt.ylabel('Normalized Energy')
+plt.ylabel('Energy (keV)')
 plt.savefig('energy_prediction.png')
 plt.close()
 
-plt.hist(tar, label='target', alpha=0.5)
+plt.hist(y_test, label='target', alpha=0.5)
 plt.hist(yhat, label='prediction', alpha=0.5)
 plt.legend(loc='upper right')
 plt.title('Event Energy Distribution')
-plt.xlabel('Normalized Energy')
+plt.xlabel('Energy (keV)')
 plt.ylabel('# Events')
 plt.savefig('hist.png')
 plt.close()
 
-# rescale
-ratio = (yhat * 1.0) / (tar * 1.0)
-tar = sio.loadmat('short_data.mat')['data'][-len(yhat):, 0, 0].astype('float64')
-yhat  = ratio * tar
+###########
+# Rescale #
+###########
+for i in range(len(yhat)):
+    mn = float(dd[x_train.shape[0] + i]['min'])
+    mx = float(dd[x_train.shape[0] + i]['max'])
+    yhat[i] = yhat[i] * (mx - mn) + mn
+
+tar = sio.loadmat('short_data.mat')['data'][-len(yhat):, 0, 0]
 
 #######################
 # Make rescaled plots #
@@ -116,7 +127,7 @@ plt.legend(loc='upper right')
 plt.title('CDMS Energy Prediction')
 plt.xlabel('Event')
 plt.ylabel('Energy (keV)')
-plt.savefig('energy_prediction_rescale.png')
+plt.savefig('rescale_energy_prediction.png')
 plt.close()
 
 plt.hist(tar, label='target', alpha=0.5)
@@ -125,5 +136,5 @@ plt.legend(loc='upper right')
 plt.title('Event Energy Distribution')
 plt.xlabel('Energy (keV)')
 plt.ylabel('# Events')
-plt.savefig('hist_rescale.png')
+plt.savefig('rescale_hist.png')
 plt.close()
